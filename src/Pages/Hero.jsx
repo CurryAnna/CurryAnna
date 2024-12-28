cb28e1599933d8a1508153b8bceb8cea1494d7f4
CurryAnna <curryanna1212@gmail.com>
2024-12-28 19:00:34 +0500

    Update Hero.jsx

--- src/Pages/Hero.jsx
+++ src/Pages/Hero.jsx
@@ -5,8 +5,8 @@ const Hero = () => {
     <div className='Hero'>
         <div className='absolute container2'>
             <div className='con1'>
-                <h1 className="welcome  text-md sm:text-xl md:text-2xl font-bold tracking-tighter text-yellow-300">Welcome to Curry Anna Book</h1>
-                <h1 className='info text-md sm:text-lg md:text-xl font-bold tracking-tighter text-yellow-300'>India's No.1 sports ID provider since 2010</h1>
+                <h1 className="welcome  text-md sm:text-xl md:text-4xl font-bold tracking-tighter text-yellow-700 font-mono">Welcome to Curry Anna Book</h1>
+                <h1 className='info text-md sm:text-lg md:text-2xl font-bold tracking-tighter text-yellow-600 font-mono'>India's No.1 sports ID provider since 2010</h1>
                 <p className='abt text-md md:text-lg font-mono tracking-tighter'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non debitis nobis aspernatur incidunt temporibus eligendi et ipsa cumque aut consequuntur.</p>
                 <div className=''>
                     <button className='btn px-2 border-2 border-yellow-200 rounded-md hover:bg-yellow-300 hover:text-black'><h1 className='mt-3'>Contact Us</h1></button>
